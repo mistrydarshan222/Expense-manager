ce58a230bfdd620eb5110b842d109b6321f4998e
mistrydarshan222 <69639961+mistrydarshan222@users.noreply.github.com>
2026-04-12 13:38:22 -0400

    implemented login with google

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:5000/api',
-  googleClientId: ''
+  googleClientId: '264247484027-7mog89oe5r9ttdcnej8ivf4p5r8uvvul.apps.googleusercontent.com'
 };
